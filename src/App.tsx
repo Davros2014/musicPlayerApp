77c3213651c7447b572e79213dd706311adf6c13
David Parsons <info@dpdesigns.eu>
2022-06-28 11:37:12 +0200

    created SongList and SongCard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const App = () => <div className="App">Music app here</div>
+const App = () => {
+  return (
+
+    <div className="App">
+        {/*Add a header here*/}
+        {/*List of songCards here - map over json data > list SongCard components*/}
+
+
+    </div>
+  )
+}
 
 export default App;
---
+++ src/components/SongCard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledSongCard } from './SongCard.styles';
+
+const SongCard = () => {
+    return (
+        <StyledSongCard>
+            Song list goes here
+        </StyledSongCard>
+    );
+};
+
+export default SongCard;
