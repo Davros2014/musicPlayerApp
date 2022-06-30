9f84cdb1e810e70d5e649d897ef536f95b218bb6
David Parsons <info@dpdesigns.eu>
2022-06-30 13:14:20 +0200

    added styling to controls - implementing functionality

--- src/components/AudioPlayer/AudioPlayer.styles.ts
+++ src/components/AudioPlayer/AudioPlayer.styles.ts
@@ -3,8 +3,15 @@ import styled from "styled-components";
 export const StyledAudioPlayer = styled.div`
   width: 100%;
   height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid rgb(0,248,196);
+  padding: 0.5rem 1.5rem;
+  border-radius: 50px;
   & .audio-controls {
-    background-color: rgba(0, 0, 0, 0);
+    font-family: 'Open Sans', sans-serif;
+    background-color: white;
     width: 100%;
     margin-top: 0;
     margin-bottom: 1rem;
@@ -13,4 +20,54 @@ export const StyledAudioPlayer = styled.div`
       margin-bottom: 0;
     }
   }
+  & div {
+      font-family: 'Open Sans', sans-serif;
+      font-weight: 700;
+      color: white;
+      font-size: 1rem;
+  }
+  & .controls-container {
+      display: flex;
+      width: 30%;
+    & .main-btn {
+      background: none;
+      width: 3rem;
+      height: 3rem;
+      border: 2px solid rgb(0,248,196);
+      border-radius: 100px;
+      margin: 0 1rem;
+      cursor: pointer;
+      color: rgb(0,248,196);
+      transition: 0.2s color ease-in-out;
+        &:hover {
+          transition: 0.2s all ease-in-out;
+          color: white;
+        }
+        &:hover > i {
+          transition: 0.2s all ease-in-out;
+          transform: scale(1.2);
+        }
+    }
+    & .control-button {
+        cursor: pointer;
+        color: rgb(0,248,196);
+        transition: 0.2s color ease-in-out;
+        &:hover {
+          transition: 0.2s color ease-in-out;
+          color: white;
+        }
+    }
+  }
+ 
+  & .progress-container {
+    display: flex;
+    width: 70%;
+    & .progress-bar {
+      background: rgb(0,248,196);
+      width: 80%;
+      border: 1px solid red;
+      padding: 0 1rem;
+    }
+  }
+  
 `;
--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { StyledAudioPlayer } from "./AudioPlayer.styles";
 
@@ -10,20 +10,27 @@ type AudioProps = {
 
 const AudioPlayer = ({ artists }: AudioProps) => {
   const {music_file_path} = artists;
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handlePlay = () => {
+    setIsPlaying(!isPlaying)
+  }  
   return (
     <StyledAudioPlayer>
-      <audio className="audio-controls" preload="metadata" data-attributecontrols src={music_file_path}>
+      <audio className="audio-controls" preload="metadata" src={music_file_path}>
         Your browser does not support the
-        <code>audio</code> element.
-        <button>Back 10 seconds</button>
-        <button>Play / Pause</button>
-        <button>Forward 10 seconds</button>
-        {/*Time */}
-        <div>0:00</div>
-        {/*progress */}
-        <input type="range" className="progress-bar"/>
-        {/*song length */}
-      </audio>
+        <code>audio</code> element.</audio>
+      <div className="controls-container">
+        <button><i className="fa-solid fa-backward control-button"></i></button>
+        <button className="main-btn" onClick={handlePlay}>{ isPlaying ? <i className="fa-solid fa-pause"></i> : <i className="fa-solid fa-play"></i>}</button>
+        <button><i className="fa-solid fa-forward control-button"></i></button>
+      </div>
+      <div className="progress-container">
+          <div>0:00</div>
+          <input type="range" className="progress-bar"/>
+          <div>1:04</div>
+      </div>
+      
     </StyledAudioPlayer>
   );
 };
